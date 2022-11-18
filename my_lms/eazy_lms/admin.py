8148dcc9b89bdf087dfe61eeef176a94ead8f623
Eazyisreal <eazyisreal192@gmail.com>
2022-11-18 10:58:49 +0100

    Docs: Document Readme.md

--- my_lms/eazy_lms/admin.py
+++ my_lms/eazy_lms/admin.py
@@ -1,3 +1,11 @@
 from django.contrib import admin
+from . import models
 
 # Register your models here.
+
+
+admin.site.register(models.Teacher)
+admin.site.register(models.Student)
+admin.site.register(models.CourseCategory)
+admin.site.register(models.Course)
+
