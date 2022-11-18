238152a8bd8cf71cf30f9cb0a00f2e8778d56593
Eazyisreal <eazyisreal192@gmail.com>
2022-11-18 10:36:03 +0100

    Feat: Add models for database storage

--- my_lms/eazy_lms/models.py
+++ my_lms/eazy_lms/models.py
@@ -1,3 +1,41 @@
 from django.db import models
 
 # Create your models here.
+# Teachers model
+
+class Teacher(models.Model):
+    full_name = models.CharField(max_length=100)
+    email = models.EmailField(max_length=100)
+    phone = models.CharField(max_length=100)
+    address = models.TextField
+    password = models.CharField(max_length=100)
+
+
+class Student(models.Model):
+    full_name = models.CharField(max_length=100)
+    email = models.EmailField(max_length=100)
+    phone = models.CharField(max_length=100)
+    address = models.CharField(max_length = 100)
+    password = models.CharField(max_length=100) 
+    interested_categories = models.TextField()
+    
+class CourseCategory(models.Model):
+    title =  models.CharField(max_length=100)
+    description: models.TextField()
+    
+    
+    
+    
+class Course(models.Model):
+    course_name = models.CharField(max_length=100)
+    course_code = models.CharField(max_length=100)
+    course_description = models.TextField()
+    course_teacher = models.ForeignKey(Teacher, on_delete=models.CASCADE)
+    course_student = models.ManyToManyField(Student)
+    course_price = models.IntegerField()
+    # course_image = models.ImageField(upload_to='course_images')
+    course_duration = models.IntegerField()
+    course_start_date = models.DateField()
+    course_end_date = models.DateField()
+    course_status = models.BooleanField(default=False)
+    category =  models.ForeignKey(CourseCategory, on_delete=models.CASCADE)
