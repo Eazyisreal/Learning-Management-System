55ebedb20ee6428d7aab7d07df46e555f820a8a6
Eazyisreal <eazyisreal192@gmail.com>
2022-12-20 17:24:32 +0100

    Feat: Added Api Token auth

--- my_lms/eazy_lms/urls.py
+++ my_lms/eazy_lms/urls.py
@@ -1,7 +1,9 @@
-from django.urls import path
+from django.urls import path, include
 from . import views
 
 
 urlpatterns =[
     path('teacher/', views.TeacherList.as_view()),
+    path('teacher/<int:pk>/', views.TeacherDetail.as_view()),
+    path('api-auth/', include('rest_framework.urls'))
 ]
--- my_lms/eazy_lms/views.py
+++ my_lms/eazy_lms/views.py
@@ -1,19 +1,23 @@
 from django.shortcuts import render
 from rest_framework.response import Response
 from rest_framework.views import APIView
+from rest_framework import generics
+from rest_framework import permissions
 from .serializers import TeacherSerializer
 from . import models
 
-# Create your views here.
-class TeacherList(APIView):
-    def get(self,request):
-        teachers = models.Teacher.objects.all()
-        serializer = TeacherSerializer(teachers,many= True)
-        return Response(serializer.data)
-        
-        
-        
-# class Studentlist(APIView):
-#     def get (self, request):
-#         students = models.Student.objects.all()
-#         serializer = 
+
+
+class TeacherList(generics.ListCreateAPIView):
+    queryset = models.Teacher.objects.all()
+    serializer_class = TeacherSerializer
+    permission_classes = [permissions.IsAuthenticated]
+    
+    
+    
+    
+class TeacherDetail(generics.RetrieveUpdateDestroyAPIView):
+    queryset = models.Teacher.objects.all()
+    serializer_class = TeacherSerializer
+    permission_classes = [permissions.IsAuthenticated]
+    
