1347ff040ff11c234eb4dc837b1b91ab7e1a7b7f
Eazyisreal <eazyisreal192@gmail.com>
2022-12-21 12:22:45 +0100

    Chore: Fixed all APIendpoit for models

--- my_lms/eazy_lms/views.py
+++ my_lms/eazy_lms/views.py
@@ -3,7 +3,7 @@ from rest_framework.response import Response
 from rest_framework.views import APIView
 from rest_framework import generics
 from rest_framework import permissions
-from .serializers import TeacherSerializer
+from .serializers import TeacherSerializer, StudentSerializer , CourseCategorySerializer, CourseSerializer
 from . import models
 
 
@@ -21,3 +21,49 @@ class TeacherDetail(generics.RetrieveUpdateDestroyAPIView):
     serializer_class = TeacherSerializer
     permission_classes = [permissions.IsAuthenticated]
     
+    
+    
+class StudentList(generics.ListCreateAPIView):
+    queryset = models.Student.objects.all()
+    serializer_class = StudentSerializer
+    permission_classes = [permissions.IsAuthenticated]
+    
+    
+    
+    
+class StudentDetail(generics.RetrieveUpdateDestroyAPIView):
+    queryset = models.Student.objects.all()
+    serializer_class = StudentSerializer
+    permission_classes = [permissions.IsAuthenticated]
+    
+    
+    
+    
+
+class CourseCategoryList(generics.ListCreateAPIView):
+    queryset = models.Student.objects.all()
+    serializer_class = CourseCategorySerializer
+    permission_classes = [permissions.IsAuthenticated]
+    
+    
+    
+    
+class CourseCategoryDetail(generics.RetrieveUpdateDestroyAPIView):
+    queryset = models.Student.objects.all()
+    serializer_class = CourseCategorySerializer
+    permission_classes = [permissions.IsAuthenticated]
+    
+    
+
+class CourseList(generics.ListCreateAPIView):
+    queryset = models.Student.objects.all()
+    serializer_class = CourseSerializer
+    permission_classes = [permissions.IsAuthenticated]
+    
+    
+    
+    
+class CourseDetail(generics.RetrieveUpdateDestroyAPIView):
+    queryset = models.Student.objects.all()
+    serializer_class = CourseSerializer
+    permission_classes = [permissions.IsAuthenticated]
