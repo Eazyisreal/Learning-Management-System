2d37e2386bde79a4356681c645ac7f72b2e92c40
Eazyisreal <eazyisreal192@gmail.com>
2022-12-21 12:32:57 +0100

    Docs: Properly commented codebase for easy retention

--- my_lms/eazy_lms/serializers.py
+++ my_lms/eazy_lms/serializers.py
@@ -1,20 +1,20 @@
 from rest_framework import serializers
 from . import models
 
-
+# TeacherSerializer
 class TeacherSerializer(serializers.ModelSerializer):
     class Meta:
         model = models.Teacher
         fields = '__all__'
         
-
+# StudentSerializer
 class StudentSerializer(serializers.ModelSerializer):
     class Meta:
         model = models.Student
         fields = '__all__'
         
         
-
+# CourseCategorySerializer
 class CourseCategorySerializer(serializers.ModelSerializer):
     class Meta:
         model = models.CourseCategory
@@ -22,7 +22,7 @@ class CourseCategorySerializer(serializers.ModelSerializer):
         
         
         
-
+# CourseSerializer
 class CourseSerializer(serializers.ModelSerializer):
     class Meta:
         model = models.Course
--- my_lms/eazy_lms/urls.py
+++ my_lms/eazy_lms/urls.py
@@ -1,12 +1,22 @@
+# import the path function from django.urls and include from my_lms/urls.py
 from django.urls import path, include
+# import views from eazy_lms/views.py
 from . import views
 
 
 urlpatterns =[
+    #  urls path for TeacherList and TeacherDetail
     path('teacher/', views.TeacherList.as_view()),
     path('teacher/<int:pk>/', views.TeacherDetail.as_view()),
+    # urls path for StudentList and StudentDetail
+    path('teacher/', views.StudentList.as_view()),
     path('student/<int:pk>/', views.StudentDetail.as_view()),
+    # urls path for CourseCategoryList and CourseCategoryDetail
+    path('coursecategory/', views.CourseCategoryList.as_view()),
     path('coursecategory/<int:pk>/', views.CourseCategoryDetail.as_view()),
+    # urls path for CourseList and CourseDetail
+    path('course/', views.CourseList.as_view()),
     path('course/<int:pk>/', views.CourseDetail.as_view()),
-    path('api-auth/', include('rest_framework.urls'))
+    # urls path for authentication
+    path('api-auth/', include('rest_framework.urls')),
 ]
