577c0cbcae840fb26521dfa155f0920ae1acf263
Eazyisreal <eazyisreal192@gmail.com>
2022-12-21 12:22:45 +0100

    Chore: Fixed all APIendpoit for models

--- my_lms/eazy_lms/urls.py
+++ my_lms/eazy_lms/urls.py
@@ -5,5 +5,8 @@ from . import views
 urlpatterns =[
     path('teacher/', views.TeacherList.as_view()),
     path('teacher/<int:pk>/', views.TeacherDetail.as_view()),
+    path('student/<int:pk>/', views.StudentDetail.as_view()),
+    path('coursecategory/<int:pk>/', views.CourseCategoryDetail.as_view()),
+    path('course/<int:pk>/', views.CourseDetail.as_view()),
     path('api-auth/', include('rest_framework.urls'))
 ]
