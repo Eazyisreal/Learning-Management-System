d647fcb08e6fa4e4473ccfa8cd7d6f560c657c0c
Eazyisreal <eazyisreal192@gmail.com>
2022-12-21 12:32:57 +0100

    Docs: Properly commented codebase for easy retention

--- my_lms/eazy_lms/views.py
+++ my_lms/eazy_lms/views.py
@@ -1,20 +1,25 @@
 from django.shortcuts import render
 from rest_framework.response import Response
+# import APIView from rest_framework
 from rest_framework.views import APIView
+# import generics from rest_framework
 from rest_framework import generics
+# import permissions from rest_framework
 from rest_framework import permissions
+# import individual serializers from serializers.py
 from .serializers import TeacherSerializer, StudentSerializer , CourseCategorySerializer, CourseSerializer
+# import individual models from models.py
 from . import models
 
 
-
+# TeacherList
 class TeacherList(generics.ListCreateAPIView):
     queryset = models.Teacher.objects.all()
     serializer_class = TeacherSerializer
     permission_classes = [permissions.IsAuthenticated]
     
     
-    
+# TeacherDetail
     
 class TeacherDetail(generics.RetrieveUpdateDestroyAPIView):
     queryset = models.Teacher.objects.all()
@@ -22,23 +27,20 @@ class TeacherDetail(generics.RetrieveUpdateDestroyAPIView):
     permission_classes = [permissions.IsAuthenticated]
     
     
-    
+# StudentList
 class StudentList(generics.ListCreateAPIView):
     queryset = models.Student.objects.all()
     serializer_class = StudentSerializer
     permission_classes = [permissions.IsAuthenticated]
     
     
-    
-    
+# StudentDetail
 class StudentDetail(generics.RetrieveUpdateDestroyAPIView):
     queryset = models.Student.objects.all()
     serializer_class = StudentSerializer
     permission_classes = [permissions.IsAuthenticated]
     
-    
-    
-    
+# CourseCategoryList 
 
 class CourseCategoryList(generics.ListCreateAPIView):
     queryset = models.Student.objects.all()
@@ -46,23 +48,21 @@ class CourseCategoryList(generics.ListCreateAPIView):
     permission_classes = [permissions.IsAuthenticated]
     
     
-    
-    
+# CourseCategoryDetail
 class CourseCategoryDetail(generics.RetrieveUpdateDestroyAPIView):
     queryset = models.Student.objects.all()
     serializer_class = CourseCategorySerializer
     permission_classes = [permissions.IsAuthenticated]
     
     
-
+# CourseList
 class CourseList(generics.ListCreateAPIView):
     queryset = models.Student.objects.all()
     serializer_class = CourseSerializer
     permission_classes = [permissions.IsAuthenticated]
     
     
-    
-    
+# CourseDetail
 class CourseDetail(generics.RetrieveUpdateDestroyAPIView):
     queryset = models.Student.objects.all()
     serializer_class = CourseSerializer
--- my_lms/my_lms/urls.py
+++ my_lms/my_lms/urls.py
@@ -5,5 +5,4 @@ from django.urls import path,include
 urlpatterns = [
     path('admin/', admin.site.urls),
      path('api/',  include('eazy_lms.urls')),
-    # path('api-auth/', include('rest_framework.urls')),
 ]
